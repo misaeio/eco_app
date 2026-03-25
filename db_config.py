b3af32b541d3350642216a3df26c557a37953f8d
misaeio <144871387+misaeio@users.noreply.github.com>
2026-03-24 22:20:26 -0500

    Refactor db_config.py comments for better clarity
    Updated comments for clarity and consistency.

--- db_config.py
+++ db_config.py
@@ -1,14 +1,15 @@
-import mysql.connector
+import mysql.connector #allows python to talk to mysql
 
 import mysql.connector
 
-def get_connection():
+def get_connection(): #defines a function called get connection
+                      #will get called if they need to connect to database
     try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",              # change if not root
-            password="YOUR REAL PASSWORD", # put your MySQL password here, replace "YOUR REAL PASSWORD"
-            database="eco_app"        # your database name
+        conn = mysql.connector.connect( #creates the connection to your localhost
+            host="localhost",          
+            user="root",              # connects to YOUR user... change if not root
+            password="YOUR REAL PASSWORD", # using YOUR password... put your MySQL password here, replace "YOUR REAL PASSWORD"
+            database="eco_app"        # your database name... the databse shared is eco_app
         )
         return conn
 
