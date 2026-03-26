4fd9a19db105b0c64938ccc9ef8f502d27b27108
misaeio <144871387+misaeio@users.noreply.github.com>
2026-03-25 22:17:11 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const backendURL = 'http://127.0.0.1:5000';
+
+//signup
+function signup(){
+    const username = document.getElementById('signup-username').value
+    const password = document.getElementById('signup-password').value
+}
