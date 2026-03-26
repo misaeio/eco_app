43b29ad8330efe1bb3f2dfaa133c4ee0dee64229
misaeio <144871387+misaeio@users.noreply.github.com>
2026-03-26 10:40:16 -0500

    fixed bug
     fetch('${backendURL}/signup',{ 
    backendURL/signup is string
    
    changed to 
     fetch(`${backendURL}/signup`,{
    
    button wouldve done nothing

--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,7 @@ function signup(){ //declares function called signup. HTML calls this
     const username = document.getElementById('signup-username').value //doc...(username') finds username input in html. .value gets the actual value
     const password = document.getElementById('signup-password').value //doc...(password') finds password input in html. .value gets the actual value
 
-    fetch('${backendURL}/signup',{ //fetch() makes a network request. ${backendURL}/signup is the URL of flask in app.py file
+    fetch(`${backendURL}/signup`,{ //fetch() makes a network request. ${backendURL}/signup is the URL of flask in app.py file
     method: 'POST', //send data to server
         headers: {'Content-Type': 'application/json' }, //tells server that data is JSON type
         body: JSON.stringify({ username, password}) //turns JS object to JSON so flask in app.py can read
