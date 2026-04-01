bdc70791d976ea8d54a8dce95892d39bd360ea83
misaeio <144871387+misaeio@users.noreply.github.com>
2026-03-31 19:53:58 -0500

    Refactor signup and login functions with comments

--- FULL_APP/frontend/app.js
+++ FULL_APP/frontend/app.js
@@ -1,21 +1,21 @@
-const backendURL = "http://127.0.0.1:5000";
+const backendURL = "http://127.0.0.1:5000"; //base URL for backend api
 
 // SIGNUP
 function signup() {
-    const username = document.getElementById('signup-username').value;
+    const username = document.getElementById('signup-username').value; //gets values from html form
     const password = document.getElementById('signup-password').value;
 
-    fetch(`${backendURL}/signup`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
+    fetch(`${backendURL}/signup`, { //send POST request to flask backend at /signup
+        method: 'POST', //POST bc we are sending info
+        headers: { 'Content-Type': 'application/json' }, //tells server this is JSON data
+        body: JSON.stringify({ username, password }) //converts user n pass to JSON
     })
-    .then(res => res.json())
+    .then(res => res.json()) //backend response to javascript object
     .then(data => {
-        if (data.message) {
+        if (data.message) { //if signup is successful display success message 
             alert(data.message);
             // Redirect with username and user_id
-            window.location.href = "dashboard.html?user_id=" + data.user_id + "&username=" + encodeURIComponent(username);
+            window.location.href = "dashboard.html?user_id=" + data.user_id + "&username=" + encodeURIComponent(username); 
         } else {
             alert(data.error);
         }
