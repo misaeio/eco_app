9a501cb2555830844657c50fad6f296334e10473
misaeio <144871387+misaeio@users.noreply.github.com>
2026-04-09 14:34:51 -0500

    Update app.js

--- FULL_APP/frontend/app.js
+++ FULL_APP/frontend/app.js
@@ -1,21 +1,21 @@
-const backendURL = "http://127.0.0.1:5000"; //base URL for backend api
+const backendURL = "http://127.0.0.1:5000";
 
 // SIGNUP
 function signup() {
-    const username = document.getElementById('signup-username').value; //gets values from html form
+    const username = document.getElementById('signup-username').value;
     const password = document.getElementById('signup-password').value;
 
-    fetch(`${backendURL}/signup`, { //send POST request to flask backend at /signup
-        method: 'POST', //POST bc we are sending info
-        headers: { 'Content-Type': 'application/json' }, //tells server this is JSON data
-        body: JSON.stringify({ username, password }) //converts user n pass to JSON
+    fetch(`${backendURL}/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
     })
-    .then(res => res.json()) //backend response to javascript object
+    .then(res => res.json())
     .then(data => {
-        if (data.message) { //if signup is successful display success message 
+        if (data.message) {
             alert(data.message);
             // Redirect with username and user_id
-            window.location.href = "dashboard.html?user_id=" + data.user_id + "&username=" + encodeURIComponent(username); 
+            window.location.href = "dashboard.html?user_id=" + data.user_id + "&username=" + encodeURIComponent(username);
         } else {
             alert(data.error);
         }
